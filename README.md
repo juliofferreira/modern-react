ac68defd0d8386c6a5e409aaaa5fcca5b7bb9b49
Julio Faria <jcesarffaria@gmail.com>
2024-05-17 16:39:40 -0300

    database setup

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] Make it deploy (w/ Vercel)
 - [x] Scaffold basic UI with mock data
 - [x] Tidy up build process
-- [ ] Actually set up a database (w/ Vercel PostgreSQL)
+- [x] Actually set up a database (w/ Vercel PostgreSQL)
 - [ ] Attach database to UI
 - [ ] Add authentication (w/ Clerk)
 - [ ] Add image upload
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
   const mockUrls = [
     "https://utfs.io/f/04b547cf-00b5-40ce-999e-7984136749fe-z0hz4s.png",
     "https://utfs.io/f/b8d78be8-3692-44b5-b288-95fd671ac813-z0hz4r.png",
@@ -10,12 +10,19 @@ export default function HomePage() {
 
   const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
 
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url} alt="image" />
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
+            <img src={image.url} />
           </div>
         ))}
       </div>
