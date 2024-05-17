0cc2375fbd77b44402f0774c5a1bd4b1dee8a9ad
Julio Faria <jcesarffaria@gmail.com>
2024-05-17 16:48:28 -0300

    force dynamic and vscode stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
+const mockUrls = [
+  "https://utfs.io/f/04b547cf-00b5-40ce-999e-7984136749fe-z0hz4s.png",
+  "https://utfs.io/f/b8d78be8-3692-44b5-b288-95fd671ac813-z0hz4r.png",
+  "https://utfs.io/f/c478236f-f031-4852-8b1a-3ac72bb8fae3-z0hz4q.png",
+  "https://utfs.io/f/eed7c53c-22ca-4894-9c16-41b49ed0d1c3-z0hz4p.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default async function HomePage() {
-  const mockUrls = [
-    "https://utfs.io/f/04b547cf-00b5-40ce-999e-7984136749fe-z0hz4s.png",
-    "https://utfs.io/f/b8d78be8-3692-44b5-b288-95fd671ac813-z0hz4r.png",
-    "https://utfs.io/f/c478236f-f031-4852-8b1a-3ac72bb8fae3-z0hz4q.png",
-    "https://utfs.io/f/eed7c53c-22ca-4894-9c16-41b49ed0d1c3-z0hz4p.png",
-  ];
-
-  const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
-
   const posts = await db.query.posts.findMany();
 
   console.log(posts);
